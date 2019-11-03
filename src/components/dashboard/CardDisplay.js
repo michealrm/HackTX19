ebef65b1540d474e3332eea87f2f363f8bd4191a
Micheal Myers <michealmyers@Micheals-MBP.lan1>
2019-11-03 05:35:04 -0600

    Proxied Marqueta Core API to avoid Access-Control-Allow-Origin

--- src/components/dashboard/CardDisplay.js
+++ src/components/dashboard/CardDisplay.js
@@ -4,7 +4,7 @@ import "./CardDisplay.css";
 const CardInfo = props => {
   const [cards, setCards] = useState([]);
 
-  fetch('https://shared-sandbox-api.marqeta.com/v3/cards/user/' + props.user_token, {
+  fetch('/cards/user/' + props.user_token, {
     method: 'post', 
     headers: new Headers({
         'Authorization': 'Basic '+btoa(process.env.REACT_APP_AUTH), 
